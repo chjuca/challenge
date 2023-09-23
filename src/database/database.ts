5ce26b13419dbd541169c6d0166316192138c47d
chjuca <chjuca@utpl.edu.ec>
2023-09-23 16:49:39 -0500

    change for url database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,15 +1,5 @@
 import { Sequelize } from 'sequelize';
 import { config } from '../config/settings';
 
-export const sequelize = new Sequelize(
 
-  process.env.POSTGRES_DB || config.POSTGRES_DB,
-  process.env.POSTGRES_USER || config.POSTGRES_USER,
-  process.env.POSTGRES_PASS || config.POSTGRES_PASS,
-  {
-    host: process.env.POSTGRES_HOST || config.POSTGRES_HOST,
-    dialect: "postgres",
-    port: 5432,
-    ssl: true
-  }
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
