d9323ce5e0a523d9f1690dbe960362dfc75b5f7f
chjuca <chjuca@utpl.edu.ec>
2023-09-23 16:52:38 -0500

    Add dialect options

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,20 @@
 import { Sequelize } from 'sequelize';
 import { config } from '../config/settings';
 
+export const sequelize = new Sequelize(
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL);
+  process.env.POSTGRES_DB || config.POSTGRES_DB,
+  process.env.POSTGRES_USER || config.POSTGRES_USER,
+  process.env.POSTGRES_PASS || config.POSTGRES_PASS,
+  {
+    host: process.env.POSTGRES_HOST || config.POSTGRES_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+      }
+    },
+  }
+);
